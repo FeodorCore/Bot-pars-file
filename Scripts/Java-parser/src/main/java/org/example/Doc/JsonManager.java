479f4ebde0c2e65dff925de5f073c1c33186b0d9
Feodor <tntgmgm@gmail.com>
2025-10-14 19:11:29 +0300

    update xml and JsonManager

--- Scripts/Java-parser/src/main/java/org/example/Doc/JsonManager.java
+++ Scripts/Java-parser/src/main/java/org/example/Doc/JsonManager.java
@@ -1,4 +1,41 @@
 package org.example.Doc;
 
-public class JsonManager {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+
+
+import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+
+public class JsonManager implements FileNameReader, DataWriter{
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+
+    @Override
+    public String readFile(String filePath){
+        try{
+            File file = new File(filePath);
+            if (!file.exists()) {
+                System.err.println("File not found: " + filePath);
+                return null;
+            }
+
+            JsonNode root = mapper.readTree(file);
+            JsonNode nameNode = root.get("name_file");
+
+            if (nameNode != null && nameNode.isTextual()) {
+                return nameNode.asText();
+            }
+            return null;
+        }catch (IOException e) {
+            System.err.println("Error reading JSON file: " + e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public void write(String date, Map<String, Map<String, String>> sheduleData, String filename){
+
+    }
 }
