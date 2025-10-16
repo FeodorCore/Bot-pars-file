40ffae6016c570e0a84da92ace247fdf9c5c482c
Feodor <tntgmgm@gmail.com>
2025-10-16 15:22:46 +0300

    update main

--- Scripts/Java-parser/src/main/java/org/example/Main.java
+++ Scripts/Java-parser/src/main/java/org/example/Main.java
@@ -1,6 +1,31 @@
 package org.example;
 
+import org.example.Doc.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            FileManager fileManager = new FileManager();
+            JsonManager jsonManager = new JsonManager();
+            YamlManager yamlManager = new YamlManager();
+            TextProcessor textProcessor = new TextProcessor();
+            ScheduleLineProcessor lineProcessor = new ScheduleLineProcessor(textProcessor);
+            Parser documentParser = new DocumentParser(textProcessor, lineProcessor);
+
+            List<DataWriter> dataWriters = Arrays.asList(jsonManager, yamlManager);
+
+            DocumentProcessor processor = new DocumentProcessor(
+                    jsonManager, documentParser, dataWriters, fileManager);
+
+            processor.processDocument();
+
+        } catch (Exception e) {
+            System.err.println("Application error: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
         }
+    }
 }
