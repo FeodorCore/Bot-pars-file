0fa90845da181dad0d5520d633012d3c6e5f6a49
Feodor <tntgmgm@gmail.com>
2025-10-20 13:56:39 +0300

    update main add record json file

--- Scripts/Python-downloader/main.py
+++ Scripts/Python-downloader/main.py
@@ -4,22 +4,29 @@ from urllib.parse import urljoin
 from pathlib import Path
 import os
 import sys
+import json
 
 class FileDownloader:
-    def __init__(self, base_url, headers, download_dir):
+    def __init__(self, base_url, headers, download_dir, json_dir):
         self.base_url = base_url
         self.headers = headers
         self.download_dir = download_dir
         self.download_dir.mkdir(parents=True, exist_ok=True)
+        self.json_dir = json_dir
+        self.json_dir.mkdir(parents=True, exist_ok=True)
 
     def download_first_file(self):
         html = self.fetch_page()
         links = self.extract_links(html)
         if not links:
-            print("Файлы не найдены.")
-            return None
+            print("files are missing.")
+            return False
         self.download_file(links[0])
-        sys.exit(2)
+        json_path = self.json_dir / "name_doc.json"
+        data = {"name_file":self.filename}
+        with open(json_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False)
+            return True
 
     def fetch_page(self):
         response = requests.get(self.base_url, headers=self.headers)
@@ -35,18 +42,17 @@ class FileDownloader:
         return [urljoin(self.base_url, a["href"]) for a in links]
 
     def download_file(self, file_url):
-        filename = os.path.basename(file_url)
-        filepath = self.download_dir / filename
+        self.filename = os.path.basename(file_url)
+        filepath = self.download_dir / self.filename
 
-        print(f"Скачиваю: {file_url}")
+        print(f"Downloads: {file_url}")
         response = requests.get(file_url, headers=self.headers, stream=True)
         response.raise_for_status()
 
         with open(filepath, "wb") as f:
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
-
-        print(f"Файл сохранён как {filepath}")
+        print(f"The file is saved as: {filepath}")
         return filepath
 
 if __name__ == "__main__":
@@ -58,6 +64,10 @@ if __name__ == "__main__":
     script_dir = Path(__file__).resolve().parent
     scripts_dir = script_dir.parent
     download_dir = scripts_dir / "Input-Output" / "Download-doc"
+    json_dir = scripts_dir / "Input-Output" / "json-file"
 
-    downloader = FileDownloader(base_url = base_url, headers=headers, download_dir=download_dir)
-    downloader.download_first_file()
+    downloader = FileDownloader(base_url = base_url, headers=headers, download_dir=download_dir, json_dir=json_dir)
+    success = downloader.download_first_file()
+    if success:
+        sys.exit(2)       
+    
